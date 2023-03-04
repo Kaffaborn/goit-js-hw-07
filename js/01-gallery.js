5fde769fede9e481fc75615ba72c688cfa72cfcb
Andrii Ivaniuk <andrey.ivanjuk@gmail.com>
2023-03-04 13:36:04 +0200

    in progress

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) { 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    }).join('');
+}
+
+function onGalleryContainerClick(evt) {
+    const isImage = evt.target.classList.contains('gallery__image');
+    if (!isImage) { 
+        return;
+    }
+
+    console.log(evt.target.dataset.source);
+}
